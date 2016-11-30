0d704b9d640a56c9c7dd6148a9c4f2928bc181af
Aaron Meredith <aaron@aaron-m.co.nz>
2016-12-01 09:46:53 +1300

    asD

---
+++ app/Entities/User.php
@@ -0,0 +1,86 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity */
+class User
+{
+    /** @ORM\Column(type="integer") */
+    private $id;
+    /** @ORM\Column(length=140) */
+    private $text;
+    /** @ORM\Column(type="datetime", name="posted_at") */
+    private $postedAt;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return User
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set postedAt
+     *
+     * @param \DateTime $postedAt
+     *
+     * @return User
+     */
+    public function setPostedAt($postedAt)
+    {
+        $this->postedAt = $postedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get postedAt
+     *
+     * @return \DateTime
+     */
+    public function getPostedAt()
+    {
+        return $this->postedAt;
+    }
+}
--- bootstrap/settings.php
+++ bootstrap/settings.php
@@ -1,9 +1,31 @@
 <?php
 
 use Monolog\Logger;
-
+# Doctrine
+# DOCTRINE_DRIVER = pdo_mysql
+# DOCTRINE_DB_NAME = localhost
+# DOCTRINE_DB_USER = root
+# DOCTRINE_DB_PASSWORD =
 return [
     'settings' => [
+
+        'doctrine' => [
+            'meta' => [
+                'entity_path' => [
+                    'app/Entity'
+                ],
+                'auto_generate_proxies' => true,
+                'proxy_dir' =>  __DIR__.'/../cache/proxies',
+                'cache' => null,
+            ],
+            'connection' => [
+                'driver'   => \env('DOCTRINE_DRIVER', "pdo_mysql"),
+                'host'     => \env('DOCTRINE_HOST', "localhost"),
+                'dbname'   => \env('DOCTRINE_DB_NAME', "slim"),
+                'user'     => \env('DOCTRINE_DB_USER', ""),
+                'password' => \env('DOCTRINE_DB_PASSWORD', ""),
+            ]
+        ],
         // IC Settings
         'determineRouteBeforeAppMiddleware' => \env('APP_DETERMINE_ROUTE_BEFORE_MIDDLEWARE', true),
         'routerCacheFile' => \env('APP_ROUTER_CACHE_FILE', false),
@@ -24,5 +46,28 @@ return [
             'path' => __DIR__ . '/../storage/logs/app.log',
             'level' => Logger::DEBUG,
         ],
+
+        'commands' => [
+                new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
+                new \Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
+
+                // ORM Commands
+                // new \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
+                new \Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
+                new \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
+                new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
+                new \Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
+                new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\InfoCommand(),
+                // new \Doctrine\ORM\Tools\Console\Command\MappingDescribeCommand()
+        ]
     ],
 ];
